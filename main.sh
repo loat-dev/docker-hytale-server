50743b1ca95c85f2b71167ce1894c0e13750441d
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-13 10:29:21 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,5 @@
+# Download and extract hytale downloader
+curl -fsSL -o ./hytale_downloader.zip https://downloader.hytale.com/hytale-downloader.zip
+mkdir -p "./hytale_downloader"
+unzip -o "./hytale_downloader.zip" -d "./hytale_downloader"
+ls -al "./hytale_downloader"
