f434cdc6decb8eb2f10fb1c460408edba3728b4c
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-13 14:31:33 +0100

    Update get_downloader.sh

--- prepare/get_downloader.sh
+++ prepare/get_downloader.sh
@@ -9,14 +9,18 @@ trap "rm -rf \"$DOWNLOADER_TEMP_DIR\"" EXIT
 echo "Downloading Downloader ..."
 echo "  Fetching downloader from \"$DOWNLOADER_URL\", saving it to \"$DOWNLOADER_TEMP_DIR/downloader.zip\""
 curl -fsSL -o "$DOWNLOADER_TEMP_DIR/downloader.zip" "$DOWNLOADER_URL"
+
 echo "Downloading Downloader ... Done!"
 
 echo "Extracting Downloader ..."
 echo "  Unzipping \"hytale-downloader-linux-amd64\" from \"$DOWNLOADER_TEMP_DIR/downloader.zip\", saving it to \"$DOWNLOADER_TEMP_DIR/hytale-downloader-linux-amd64\""
 unzip -q "$DOWNLOADER_TEMP_DIR/downloader.zip" "hytale-downloader-linux-amd64" -d "$DOWNLOADER_TEMP_DIR"
+
 echo "  Moving \"$DOWNLOADER_TEMP_DIR/hytale-downloader-linux-amd64\" to \"./src/downloader\""
 mv "$DOWNLOADER_TEMP_DIR/hytale-downloader-linux-amd64" "./src/downloader"
+
 chmod +x "./src/downloader"
+
 echo "Extracting Downloader ... Done!"
 
 rm -rf "$DOWNLOADER_TEMP_DIR"
