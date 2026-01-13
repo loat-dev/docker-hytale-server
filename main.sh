51daab92426da6a07294998541c9485f7a5a92af
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-13 10:31:13 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 # Download and extract hytale downloader
 curl -fsSL -o ./hytale_downloader.zip https://downloader.hytale.com/hytale-downloader.zip
 mkdir -p "./hytale_downloader"
-unzip -o "./hytale_downloader.zip" -d "./hytale_downloader"
+unzip "./hytale_downloader.zip" "hytale-downloader-linux-amd64" -d "./hytale_downloader"
 ls -al "./hytale_downloader"
