6385c30285b69fee4b7eda3d4dc11ef0c3cc94f5
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-13 14:32:44 +0100

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -4,7 +4,7 @@ set -e
 DOWNLOADER_TEMP_DIR="$(mktemp -d)"
 
 echo "Run Downloader..."
-"/app/downloader" -download-path "$DOWNLOADER_TEMP_DIR/server.zip"
+"/app/downloader" -skip-update-check -download-path "$DOWNLOADER_TEMP_DIR/server.zip"
 
 unzip -q "$DOWNLOADER_TEMP_DIR/server.zip" -d "$DOWNLOADER_TEMP_DIR/server"
 ls -al "."
