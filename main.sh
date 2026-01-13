e5634dce0fc08a1bac04f69c0ef9a5d3ea939d5b
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-13 12:23:38 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
+set -e
 
 DOWNLOADER_URL="https://downloader.hytale.com/hytale-downloader.zip"
 
 DOWNLOADER_TEMP_DIR="$(mktemp -d)"
 trap 'rm -rf "$DOWNLOADER_TEMP_DIR"' EXIT
 
-# Download, extract and run hytale downloader
-cd "$DOWNLOADER_TEMP_DIR"
-
 echo "Downloading Downloader..."
-curl -fsSL -o "./downloader.zip" "$DOWNLOADER_URL"
+curl -fsSL -o "$DOWNLOADER_TEMP_DIR/downloader.zip" "$DOWNLOADER_URL"
 echo "Downloading Downloader... Done!"
 
 echo "Extracting Downloader..."
-unzip -q -d "./" "./downloader.zip" "hytale-downloader-linux-amd64"
-mv "./hytale-downloader-linux-amd64" "./downloader"
-chmod +x "./downloader"
+unzip -q "$DOWNLOADER_TEMP_DIR/downloader.zip" "hytale-downloader-linux-amd64" -d "$DOWNLOADER_TEMP_DIR"
+mv "$DOWNLOADER_TEMP_DIR/hytale-downloader-linux-amd64" "$DOWNLOADER_TEMP_DIR/downloader"
+chmod +x "$DOWNLOADER_TEMP_DIR/downloader"
 echo "Extracting Downloader... Done!"
 
-cd -
-
 echo "Run Downloader..."
-./downloader -download-path "./server.zip"
-unzip -q -d "./server" "$DOWNLOADER_TEMP_DIR/server.zip"
+"$DOWNLOADER_TEMP_DIR/downloader" -download-path "$DOWNLOADER_TEMP_DIR/server.zip"
+
+mkdir -p "./server"
+unzip -q "$DOWNLOADER_TEMP_DIR/server.zip" -d "./server"
 ls -al "./server"
 echo "Run Downloader... Done!"
 
