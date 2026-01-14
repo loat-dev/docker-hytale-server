0c5d96abc8ce9f52c98d76251a49da013cf5f317
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-14 20:18:44 +0100

    fix: refactor

--- src/main.sh
+++ src/main.sh
@@ -1,37 +1,23 @@
 #!/bin/sh
 set -e
 
-rm -f "/app/.healthy"
+./health/set_unhealthy.sh
 
-DOWNLOADER_TEMP_DIR="$(mktemp -d)"
-
-echo "Run Downloader ..."
-echo "  Running downloader, saving it to \"$DOWNLOADER_TEMP_DIR/server.zip\""
-"/app/downloader" \
-  -skip-update-check \
-  -credentials-path "./.hytale-downloader-credentials.json" \
-  -download-path "$DOWNLOADER_TEMP_DIR/server.zip"
-
-echo "  Unzipping files from \"$DOWNLOADER_TEMP_DIR/server.zip\", saving it to \"$DOWNLOADER_TEMP_DIR/server/...\""
-unzip -q "$DOWNLOADER_TEMP_DIR/server.zip" -d "$DOWNLOADER_TEMP_DIR/server"
-
-echo "  Copying server files \"$DOWNLOADER_TEMP_DIR/server/Server/...\" to \"/data/...\""
-cp -r "$DOWNLOADER_TEMP_DIR/server/Server/." "/data/"
-
-echo "  Copying asset files \"$DOWNLOADER_TEMP_DIR/server/Assets.zip\" to \"/data/assets.zip\""
-cp -r "$DOWNLOADER_TEMP_DIR/server/Assets.zip" "/data/assets.zip"
-
-echo "  Cleaning up"
-rm -rf "$DOWNLOADER_TEMP_DIR"
-
-echo "Run Downloader ... Done!"
+if [ "$DOWNLOAD_NEW_FILES_ON_START" = "true" ] || [ ! -f "./HytaleServer.jar" ] || [ ! -f "./assets.zip" ]; then
+  ./run_downloader.sh \
+    -credentials-path "./.hytale-downloader-credentials.json" \
+    -download-path "./assets.zip"
+else
+  echo "Skipping Downloader, using existing files"
+fi
 
 echo "Set container to \"healthy\""
-touch "/app/.healthy"
+./health/set_healthy.sh
 
 echo "Run Server ..."
 java \
   -XX:AOTCache="./HytaleServer.aot" \
   -jar "./HytaleServer.jar" \
   --assets "./assets.zip" \
-  --bind "0.0.0.0:$SERVER_PORT"
+  --bind "0.0.0.0:$PORT" \
+  $EXTRA_ARGS
---
+++ src/run_downloader.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+DOWNLOADER_TEMP_DIR="$(mktemp -d)"
+
+echo "Run Downloader ..."
+echo "  Running downloader, saving it to \"$DOWNLOADER_TEMP_DIR/server.zip\""
+/app/downloader \
+  -skip-update-check \
+  -credentials-path "./.hytale-downloader-credentials.json" \
+  -download-path "$DOWNLOADER_TEMP_DIR/server.zip"
+
+echo "  Unzipping files from \"$DOWNLOADER_TEMP_DIR/server.zip\", saving it to \"$DOWNLOADER_TEMP_DIR/server/...\""
+unzip -q "$DOWNLOADER_TEMP_DIR/server.zip" -d "$DOWNLOADER_TEMP_DIR/server"
+
+echo "  Copying server files \"$DOWNLOADER_TEMP_DIR/server/Server/...\" to \"/data/...\""
+cp -r "$DOWNLOADER_TEMP_DIR/server/Server/." "/data/"
+
+echo "  Copying asset files \"$DOWNLOADER_TEMP_DIR/server/Assets.zip\" to \"/data/assets.zip\""
+cp -r "$DOWNLOADER_TEMP_DIR/server/Assets.zip" "/data/assets.zip"
+
+echo "  Cleaning up"
+rm -rf "$DOWNLOADER_TEMP_DIR"
+
+echo "Run Downloader ... Done!"
